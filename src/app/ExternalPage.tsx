41bac49c75e875a8b54c6a9404e0bb32c7b42178
Santiago Zappa <santiagozappab@gmail.com>
2025-01-03 00:35:07 -0500

    hotmart 2

--- src/app/ExternalPage.tsx
+++ src/app/ExternalPage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const ExternalPage = () => {
   return (
-    <div style={{ width: '100%', height: '190vh', overflow: 'hidden' }}>
+    <div style={{ width: '100%', height: '250vh', overflow: 'hidden' }}>
       <iframe
         src="https://hotm.art/gohRuaB"
         width="100%"
