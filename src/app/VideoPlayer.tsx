cfb5fd71f8848064b1eb7552a211c2cc23b5a5f5
Santiago Zappa <santiagozappab@gmail.com>
2025-01-06 09:40:25 -0500

    VSL Actualizado 15 dias +

--- src/app/VideoPlayer.tsx
+++ src/app/VideoPlayer.tsx
@@ -4,7 +4,7 @@ const VideoPlayer = () => {
   useEffect(() => {
     // Cargar el script solo una vez cuando el componente se monta
     const script = document.createElement('script');
-    script.src = 'https://scripts.converteai.net/2ffe5a85-c74f-4a8b-a275-5f727ac3597d/players/676a1e64572299dbc2d8f8cc/player.js';
+    script.src = 'https://scripts.converteai.net/61a3f07e-18b1-4742-b769-32c6e9519df2/players/677be7d41beda31ebad14cb9/player.js';
     script.async = true;
     document.head.appendChild(script);
 
@@ -15,23 +15,23 @@ const VideoPlayer = () => {
   }, []); // El array vacío significa que este efecto solo se ejecuta una vez
 
   return (
-    <div id="vid_676a1e64572299dbc2d8f8cc" style={{ position: 'relative', width: '100%', padding: '56.25% 0 0' }}>
-      <img 
-        id="thumb_676a1e64572299dbc2d8f8cc"
-        src="https://images.converteai.net/2ffe5a85-c74f-4a8b-a275-5f727ac3597d/players/676a1e64572299dbc2d8f8cc/thumbnail.jpg"
+    <div id="vid_677be7d41beda31ebad14cb9" style={{ position: 'relative', width: '100%', padding: '56.25% 0 0' }}>
+      <img
+        id="thumb_677be7d41beda31ebad14cb9"
+        src="https://images.converteai.net/61a3f07e-18b1-4742-b769-32c6e9519df2/players/677be7d41beda31ebad14cb9/thumbnail.jpg"
         alt="thumbnail"
         style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', display: 'block' }}
       />
-      <div 
-        id="backdrop_676a1e64572299dbc2d8f8cc" 
-        style={{ 
-          WebkitBackdropFilter: 'blur(5px)', 
-          backdropFilter: 'blur(5px)', 
-          position: 'absolute', 
-          top: 0, 
-          height: '100%', 
-          width: '100%' 
-        }} 
+      <div
+        id="backdrop_677be7d41beda31ebad14cb9"
+        style={{
+          WebkitBackdropFilter: 'blur(5px)',
+          backdropFilter: 'blur(5px)',
+          position: 'absolute',
+          top: 0,
+          height: '100%',
+          width: '100%',
+        }}
       />
     </div>
   );
