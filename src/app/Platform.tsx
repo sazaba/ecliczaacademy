6749409d2a53cdc04f7470b2a745171ecdd03e5a
Santiago Zappa <santiagozappab@gmail.com>
2025-02-26 12:01:20 -0500

    platform

--- src/app/Platform.tsx
+++ src/app/Platform.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import Hp from './images/Hp.webp';
+import TP from './images/TP.webp';
 
 const Platform: React.FC = () => {
   return (
@@ -33,7 +33,7 @@ const Platform: React.FC = () => {
         <div className="relative max-w-4xl mx-auto p-4 bg-white rounded-3xl shadow-2xl">
           <div className="relative overflow-hidden rounded-3xl">
             <Image
-              src={Hp}
+              src={TP}
               alt="Vista previa de nuestra plataforma de aprendizaje"
               layout="responsive"
               width={1920} // Proporción 16:9
