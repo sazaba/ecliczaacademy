39841d8af08d27dcf99088338fa423ac96331fb2
Santiago Zappa <santiagozappab@gmail.com>
2025-03-03 22:52:56 -0500

    new vturb

--- src/app/VideoPlayer.tsx
+++ src/app/VideoPlayer.tsx
@@ -4,7 +4,7 @@ const VideoPlayer = () => {
   useEffect(() => {
     // Cargar el script solo una vez cuando el componente se monta
     const script = document.createElement('script');
-    script.src = 'https://scripts.converteai.net/3687d5dc-9e85-4979-bb5e-c7c7bbf7f030/players/67b4a22ff069f7ce83a24837/player.js';
+    script.src = 'https://scripts.converteai.net/fd8b82e3-999d-46ce-8650-2e66bb34b4e4/players/67c67663c37d6e3e2f8bd53a/player.js';
     script.async = true;
     document.head.appendChild(script);
 
@@ -15,15 +15,15 @@ const VideoPlayer = () => {
   }, []); // El array vacío significa que este efecto solo se ejecuta una vez
 
   return (
-    <div id="vid_67b4a22ff069f7ce83a24837" style={{ position: 'relative', width: '100%', padding: '56.25% 0 0' }}>
+    <div id="vid_67c67663c37d6e3e2f8bd53a" style={{ position: 'relative', width: '100%', padding: '56.25% 0 0' }}>
       <img
-        id="thumb_67b4a22ff069f7ce83a24837"
-        src="https://images.converteai.net/3687d5dc-9e85-4979-bb5e-c7c7bbf7f030/players/67b4a22ff069f7ce83a24837/thumbnail.jpg"
+        id="thumb_67c67663c37d6e3e2f8bd53a"
+        src="https://images.converteai.net/fd8b82e3-999d-46ce-8650-2e66bb34b4e4/players/67c67663c37d6e3e2f8bd53a/thumbnail.jpg"
         alt="thumbnail"
         style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', display: 'block' }}
       />
       <div
-        id="backdrop_67b4a22ff069f7ce83a24837"
+        id="backdrop_67c67663c37d6e3e2f8bd53a"
         style={{
           WebkitBackdropFilter: 'blur(5px)',
           backdropFilter: 'blur(5px)',
@@ -38,3 +38,4 @@ const VideoPlayer = () => {
 };
 
 export default VideoPlayer;
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,10 +52,10 @@ export default function RootLayout({
           />
         </noscript>
         
-        <link rel="preload" href="https://scripts.converteai.net/3687d5dc-9e85-4979-bb5e-c7c7bbf7f030/players/67b4a22ff069f7ce83a24837/player.js" as="script"/>
+        <link rel="preload" href="https://scripts.converteai.net/fd8b82e3-999d-46ce-8650-2e66bb34b4e4/players/67c67663c37d6e3e2f8bd53a/player.js" as="script"/>
 <link rel="preload" href="https://scripts.converteai.net/lib/js/smartplayer/v1/smartplayer.min.js" as="script"/>
-<link rel="preload" href="https://images.converteai.net/3687d5dc-9e85-4979-bb5e-c7c7bbf7f030/players/67b4a22ff069f7ce83a24837/thumbnail.jpg" as="image"/>
-<link rel="preload" href="https://cdn.converteai.net/3687d5dc-9e85-4979-bb5e-c7c7bbf7f030/67b4a1fd54eeec1f2f9c482b/main.m3u8" as="fetch"/>
+<link rel="preload" href="https://images.converteai.net/fd8b82e3-999d-46ce-8650-2e66bb34b4e4/players/67c67663c37d6e3e2f8bd53a/thumbnail.jpg" as="image"/>
+<link rel="preload" href="https://cdn.converteai.net/fd8b82e3-999d-46ce-8650-2e66bb34b4e4/67c6763a84cea79bcc3d8ace/main.m3u8" as="fetch"/>
 <link rel="dns-prefetch" href="https://cdn.converteai.net"/>
 <link rel="dns-prefetch" href="https://scripts.converteai.net"/>
 <link rel="dns-prefetch" href="https://images.converteai.net"/>
